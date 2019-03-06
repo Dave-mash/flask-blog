93f7ec525de548969794034ed96f2cc98688ca1f
Dave-mash <macharia3041@gmail.com>
2019-03-06 11:41:42 +0300

    Playing around with emitters

--- server/server.js
+++ server/server.js
@@ -12,11 +12,29 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath))
 
+// io.emit works with every connection whereas 
+// socket.emit works with single connections
+
 io.on('connection', (socket) => {
     console.log('New user connected!')
     
     socket.on('disconnect', () => {
         console.log('Disconnected from server!')
+    });
+
+    // socket.emit('newMessage', {
+    //     from: "John",
+    //     text: "This is from John",
+    //     createdAt: 'today'
+    // });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message)
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 });
 
